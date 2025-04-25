24baa80825970b04d57af06a75fe649c9fb4543f
sarujan1 <sarujan@codelantic.com>
2025-04-25 14:51:29 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import Navbar from './component/navbar'
-import Hero from './component/hero'
-import Highlights from './component/highlights'
-import Model from './component/model'
+import React from 'react';
+import Navbar from './component/navbar';
+import Hero from './component/hero';
+import Highlights from './component/highlights';
+import Model from './component/model';
+
+import * as Sentry from "@sentry/react";
 
 const App = () => {
+
+  // return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
+
   return (
     <main className='bg-black'>
       <Navbar/>
@@ -15,4 +20,4 @@ const App = () => {
   )
 }
 
-export default App;
+export default Sentry.withProfiler(App);
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,27 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://969553321bce87e1c3bb48dc67784f34@o4509211825012736.ingest.us.sentry.io/4509211826061312",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  integrations: [
+    // Sentry.browserTracingIntegration(),
+    // Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.replayIntegration({
+      // Additional SDK configuration goes in here, for example:
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  // tracesSampleRate: 1.0, 
+  // tracePropagationTargets: ["localhost"],
+  // replaysSessionSampleRate: 0.1,
+  // replaysOnErrorSampleRate: 1.0, 
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
